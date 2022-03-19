d0887af572402f8e9360958e82ec09d082e5bb13
mihkelvaher <mihkel.vaher@industry62.com>
2022-03-19 17:11:24 +0200

    Objektid

--- klassid/src/main/java/ee/mihkel/klassid/Tellimus.java
+++ klassid/src/main/java/ee/mihkel/klassid/Tellimus.java
@@ -1,12 +1,20 @@
 package ee.mihkel.klassid;
 
+// komplekt väärtustest, mis võivad olla igal klassil erinevad
 public class Tellimus {
     String tellija;
     int kogusumma;
     boolean makstud;
 
+    // new Tellimus("Kaarel", 99, false); Tellimus@312fasd12
+    // new Tellimus("Malle", 88, false); Tellimus@sada12312
+    // new Tellimus("Karel",77, false); Tellimus@q123asdq
+
+    // foreach -- if (makstud == false) {näita välja}
+
                     // luua saab uut tellimust ainult kui sulgude sisene on täidetud
                     // new Tellimus("",0,false);
+    // constructor
     public Tellimus(String _tellija, int _kogusumma, boolean _makstud) {
         this.tellija = _tellija;
         this.kogusumma = _kogusumma;
@@ -17,13 +25,19 @@ public class Tellimus {
     //public Tellimus() {
     // }
 
+    // by Default on kõik klassid väljakuvades mälukohaga
+    // ee.mihkel.klassid.Tellimus@71aa2f2
 
+    // kui kuvame otse backendist brauserisse (ei kasuta frontendi)
+    // siis tema väärtuste nägemiseks peame tegema toString() funktsiooni
+    // kirjutab üle kuidas ta varasemalt näitas  (ee.mihkel.klassid.Tellimus@71aa2f2)
+    // ja näidatakse nüüd niimoodi nagu on "return" taga kirjas (return "Tellimus{" + .....)
     @Override
     public String toString() {
-        return "Tellimus{" +
-                "tellija='" + tellija + '\'' +
-                ", kogusumma=" + kogusumma +
-                ", makstud=" + makstud +
+        return "{" +
+                "tellija:'" + tellija + '\'' +
+                ", kogusumma:" + kogusumma +
+                ", makstud:" + makstud +
                 '}';
     }
 }
